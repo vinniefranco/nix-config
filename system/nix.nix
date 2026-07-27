e2cfa930bd1d6fbd8cafb3f4144525eaab1b2d00
vinniefranco <vince@freshivore.net>
2026-07-27 11:15:07 -0500

    Removing vesktop electron pin workaround

--- system/nix.nix
+++ system/nix.nix
@@ -53,9 +53,4 @@
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
-
-  # vesktop still pins electron 40 (EOL); allow it until nixpkgs bumps it
-  nixpkgs.config.permittedInsecurePackages = [
-    "electron-40.10.5"
-  ];
 }
